200dea83c5ccaf07718894900864284e304dd9f4
chaun14 <chaun1403@gmail.com>
2020-04-29 17:14:57 +0200

    Rename mis-spelled function

--- modules/db.js
+++ modules/db.js
@@ -10,13 +10,13 @@ var dbinfos = {
     charset: 'utf8mb4_bin'
 };
 
-function handleDisconnect() {
+function mysqlConnect() {
     console.log("Connexion à la bdd")
     connection = mysql.createConnection(dbinfos); // Recreate the connection, since the old one cannot be reused.
     connection.connect(function onConnect(err) { // The server is either down
         if (err) { // or restarting (takes a while sometimes).
             console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect, 10000); // We introduce a delay before attempting to reconnect,
+            setTimeout(mysqlConnect, 10000); // We introduce a delay before attempting to reconnect,
         }
         console.log('connected as id ' + connection.threadId);
         // to avoid a hot loop, and to allow our node script to
@@ -25,13 +25,13 @@ function handleDisconnect() {
     connection.on('error', function onError(err) {
         console.log('db error', err);
         if (err.code == 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-            handleDisconnect(); // lost due to either server restart, or a
+            mysqlConnect(); // lost due to either server restart, or a
         } else { // connnection idle timeout (the wait_timeout
             throw err; // server variable configures this)
         }
     });
 }
-handleDisconnect();
+mysqlConnect();
 
 
 
